a6699de795e892a48f72a93b0f85cee66bc157dc
Amr <amrelbialy26@gmail.com>
2020-06-26 16:04:15 +0200

    before launch

--- src/Trips/components/Header.js
+++ src/Trips/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../css/style.css";
 import img from "../../img/cover3.jpg";
-import img1 from "../../img/cover1.jpg";
+import img1 from "../../img/newcover2.jpg";
 
 const Header = props => {
   return (
